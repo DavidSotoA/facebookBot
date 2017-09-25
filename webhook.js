dec11b4d49230be8ce2b082395aa6a0dec71b502
David Soto <davidsoto17011@gmail.com>
2017-09-25 17:49:41 -0500

    chat 1.0

--- webhook.js
+++ webhook.js
@@ -1,12 +1,11 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
-      fs = require('fs'),
-      http = require('http'),
-      https = require('https');
+      request = require('request'),
+      fs = require('fs');
 
 var app = express();
-var port_http = 443,
-    port_https = 80;
+var port = 3000;
+var PAGE_ACCESS_TOKEN = "EAAa92vnpIT0BAFfOn9nq8mFjsL22YvbKQCr88RgYRulNmFcORlye6yg5L3BOFgtzSiLtZCz9dbyOls6oajybAZAwhE3PGxLVj9pQTJDkiFX4XjKnx2QxGNroQDWWYKWZCshnJx0P6DQpDSl3TlmzeVzioCGEgZBbDd9TvJf5tgZDZD";
 
 var options = {
   key: fs.readFileSync('ssl/client-key.pem', 'utf8'),
@@ -16,34 +15,186 @@ var options = {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-http.createServer(app).listen(port_http , () => {
-    console.log('Express server http listening on port %d in %s mode', port_http, app.settings.env);
-});
-
-https.createServer(options, app).listen(port_https, () =>{
-     console.log('Express server https listening on port %d in %s mode', port_https, app.settings.env);
+app.get("/",function(req, res){
+    res.send("Hola");
 });
 
 /* For Facebook Validation */
-app.get('/webhook', (req, res) => {
-  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'tuxedo_cat') {
-    res.status(200).send(req.query['hub.challenge']);
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] == 'hola') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']).end();
   } else {
-    res.status(403).end();
-  }
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403).end();          
+  }  
 });
 
 /* Handling all messenges */
-app.post('/webhook', (req, res) => {
-  console.log(req.body);
-  if (req.body.object === 'page') {
-    req.body.entry.forEach((entry) => {
-      entry.messaging.forEach((event) => {
-        if (event.message && event.message.text) {
-          sendMessage(event);
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+  console.log(JSON.stringify(data));
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else if (event.postback) {
+          receivedPostback(event); 
+        } else {
+          console.log("Webhook received unknown event: ", event);
         }
       });
     });
-    res.status(200).end();
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
   }
+});
+
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  // The 'payload' param is a developer-defined field which is set in a postback 
+  // button for Structured Messages. 
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " + 
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  // When a postback is called, we'll send a message back to the sender to 
+  // let them know it was successful
+  sendTextMessage(senderID, "Postback called");
+}
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+function sendGenericMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",               
+            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",               
+            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  };  
+
+  callSendAPI(messageData);
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
+}
+
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!')
 });
